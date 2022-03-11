174490ff9e9f835025087b3fbb3538804f989369
Manuel Gonçalves <mjgoncalves.trabalho@gmail.com>
2022-03-11 15:32:24 +0000

    Initial grid squares 16x16

--- js/script.js
+++ js/script.js
@@ -1,1 +1,16 @@
 const container = document.querySelector('.container');
+
+let rows = 16;
+let columns = 16;
+
+let gridSize = new Array(rows * columns);
+
+for (let i = 0; i < gridSize.length; i++) {
+  gridSize[i] = document.createElement('div');
+  gridSize[i].classList.add('gridItem');
+
+  // gridSize[i].textContent = `${i}`;
+
+  gridSize[i].textContent = 'grid-cell';
+  container.appendChild(gridSize[i]);
+}
